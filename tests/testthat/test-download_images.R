678a092a4748e7a088088cc6766c16a0b4a2c0b4
nfox29 <nf2g13@soton.ac.uk>
2019-04-24 18:30:36 +0100

    - skip download test on cran and travis

--- tests/testthat/test-download_images.R
+++ tests/testthat/test-download_images.R
@@ -6,6 +6,9 @@ test_that("fails correctly", {
 
 
 test_that("output is correct", {
+  skip_on_travis()
+  skip_on_cran()
+
   download_test <- download_images(photo_id = 47259127482, saveDir = "test_images", api_key = "3863b3b3d95341cff763f05e989012b7")
 
   expect_equal(file.exists(".\\test_images\\47259127482_05d7096ed3_o.jpg"), TRUE)
